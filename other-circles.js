01c2aeffca8c1fa8a6cc08397c21a8c82af79594
wbogusky <47305769+wbogusky@users.noreply.github.com>
2019-02-22 17:14:18 -0800

    Create other-circles.js

---
+++ other-circles.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+var x = []; // new empty array
+var y = []; // new empty array
+
+function draw() {
+  background(255);
+  noFill();
+
+  x.push(mouseX); // equivalent to append(x, mouseX)
+  y.push(mouseY); // equivalent to append(y, mouseY)
+
+  for (var i = 0; i < x.length; i = i + 5) {
+    beginShape();
+    vertex(x[i]+ i, y[i] + i);
+    vertex(x[i]+ i, y[i]);
+    vertex(x[i], y[i] + i);
+    endShape(CLOSE);
+  }
+
+  x = x.slice(-50); // keep the last 50 x values
+  y = y.slice(-50); // keep the last 50 y values
+}
